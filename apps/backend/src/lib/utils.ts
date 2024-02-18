4e191c15dcd9342ef917bb2f13004413ec4d5226
Cole Gawin <colegawin@gmail.com>
2024-02-17 23:07:16 -0800

    fix backend

--- apps/backend/src/lib/utils.ts
+++ apps/backend/src/lib/utils.ts
@@ -1,6 +1,11 @@
+import { ConvexHttpClient } from "convex/browser";
 import OpenAI from "openai";
+
+export { api } from "../../../../convex/_generated/api";
 
 export const openai = new OpenAI({
   apiKey: process.env["TOGETHER_API_KEY"],
   baseURL: "https://api.together.xyz",
 });
+
+export const convex = new ConvexHttpClient(process.env.CONVEX_URL as string);
